99c6cd6e62616ba56815914f0a9f3df51d6c13ee
Stefan Muzyka <nashes1320@gmail.com>
2023-04-08 17:36:30 +0300

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ButtonAddAppointClick(object sender, EventArgs e)
+        {
+            MainSheet.Text = "AppointButton is Clicked!";
+        }
+
+        public void ButtonAddTodoClick(object sender, EventArgs e) 
+        {
+            MainSheet.Text = "TodoButton is Clicked";
+            MainSheet.TextColor = Color.Aquamarine;
+        }
+
+        public void ButtonClearNoteClick(object sender, EventArgs e) 
+        {
+            MainSheet.Text = "nothing";
+            MainSheet.TextColor = Color.Black;
+        }
+
+        public void ButtonInfoClick(object sender, EventArgs e) 
+        {
+            MainSheet.TextColor = Color.Aqua;
+        }
+
+    }
+}
